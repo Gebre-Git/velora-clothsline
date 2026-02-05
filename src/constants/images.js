5c9913284c0b84bb37c94c5238695f663c37526c
Gebre-Git <gebreone777@gmail.com>
2026-02-05 11:57:29 +0300

    fix image related issue

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,16 +1,16 @@
 // src/constants/images.js
 import clothline from '../assets/clothline.jpg';
-import clothline from '../assets/clothline 3.jpg';
-import clothline from '../assets/step one.jpg';
-import clothline from '../assets/step two.jpg';
-import clothline from '../assets/step three.jpg';
-// import shirt1 from '../assets/shirt1.jpg'; <--- Add more as you download them
+import clothline3 from '../assets/clothline 3.jpg';
+import stepOne from '../assets/step one.jpg';
+import stepTwo from '../assets/step two.jpg';
+import stepThree from '../assets/step three.jpg';
+// import shirt1 from '../assets/shirt1.jpg'; <-- Add more as you download them
 
 export const IMAGES = {
     Ulitimate_use: clothline,
-    Upgrade_space: clothline,
-    step_one: clothline,
-    step_two: clothline,
-    step_three: clothline,
+    Upgrade_space: clothline3,
+    step_one: stepOne,
+    step_two: stepTwo,
+    step_three: stepThree,
     // SHIRT_ONE: shirt1,
 };
---
+++ src/constants/images.ts
@@ -0,0 +1,21 @@
+import clothline from '../assets/clothline.jpg';
+import clothline3 from '../assets/clothline 3.jpg';
+import stepOne from '../assets/step one.jpg';
+import stepTwo from '../assets/step two.jpg';
+import stepThree from '../assets/step three.jpg';
+
+export const IMAGES: {
+  Ulitimate_use: string;
+  Upgrade_space: string;
+  step_one: string;
+  step_two: string;
+  step_three: string;
+} = {
+  Ulitimate_use: clothline,
+  Upgrade_space: clothline3,
+  step_one: stepOne,
+  step_two: stepTwo,
+  step_three: stepThree,
+};
+
+export default IMAGES;
